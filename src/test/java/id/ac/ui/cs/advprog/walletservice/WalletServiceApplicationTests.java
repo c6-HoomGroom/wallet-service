5d765d3b264fe316635a2491ce87e5769b074e78
@dylan.dahran <dylan.dahran@ui.ac.id>
2024-04-17 18:34:30 +0700

    initialization

---
+++ src/test/java/id/ac/ui/cs/advprog/walletservice/WalletServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.walletservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
